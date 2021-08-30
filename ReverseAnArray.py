363aa4410c170813676bc0cb33726f5962d78b1b
Rituja Shinde <65076102+ritujashinde97@users.noreply.github.com>
2021-08-30 13:45:19 +0530

    Add files via upload

---
+++ ReverseAnArray.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def reverseArray(A, start, end):
+    while start < end:
+        A[start],A[end] = A[end],A[start]
+        start += 1
+        end-=1
+        
+A = [1,2,3,4,5,6]
+print(A)
+
+reverseArray(A,0,5)
+print("Reversed list is")
+print(A)
